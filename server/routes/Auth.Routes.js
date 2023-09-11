12b1b896dc1b72418d6b77207db3c0deb627b7ee
sido69 <yoann.croguennec@gmail.com>
2023-09-11 05:25:21 +0200

    first commit

--- server/routes/Auth.Routes.js
+++ server/routes/Auth.Routes.js
@@ -4,6 +4,4 @@ const authCtrl = require('../controllers/AuthCtrl')
 router.post("/signup", authCtrl.signup)
 router.post("/login", authCtrl.login)
 
-// router.delete("/fkjezejf", () => {});
-
 module.exports = router;
